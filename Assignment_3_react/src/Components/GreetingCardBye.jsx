380b191be451041a43960f0a3eb940ab710c7c33
anubhav0914 <anubhavg.cse22@gmail.com>
2025-05-02 23:29:38 +0530

    adding unmount

--- Assignment_3_react/src/Components/GreetingCardBye.jsx
+++ Assignment_3_react/src/Components/GreetingCardBye.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export default class GreetingCardBye extends Component {
+
+  componentWillUnmount() {
+    console.log("GreetingCardHello is unmounting");
+    alert("Goodbye from GreetingCardHello");
+  }
+  
   render() {
     return (
         <div className='greeting'>
